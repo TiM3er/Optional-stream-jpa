92187930fa2e1325f9923a71ac41661d0309ce30
pwitowski <pwitowski@omecon.pl>
2023-10-30 18:25:29 +0100

    init commit #2

---
+++ optional-stream-jpa/src/main/java/pl/tim3erland/omecon/code/review/optionalstreamjpa/OptionalStreamJpaApplication.java
@@ -0,0 +1,13 @@
+ package pl.tim3erland.omecon.code.review.optionalstreamjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalStreamJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionalStreamJpaApplication.class, args);
+	}
+
+}
---
+++ optional-stream-jpa/src/test/java/pl/tim3erland/omecon/code/review/optionalstreamjpa/OptionalStreamJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tim3erland.omecon.code.review.optionalstreamjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionalStreamJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
